0590f542dfe97e479a59f4208d813e7bf6910042
raviteja <ravitejatfcdemo@gmail.com>
2023-01-25 14:19:36 +0530

    commit sample jenkins job

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,39 +1,41 @@
 def call() {
 
-    pipeline {
-        agent {
-            label 'work-station'
+  pipeline {
+    agent {
+      label 'work-station'
+    }
+
+    stages {
+
+      stage('Compile/Build') {
+        steps {
+          script {
+            common.compile()
+          }
         }
+      }
 
-        stages {
-
-            stage('compile') {
-                steps {
-                    echo 'compile'
-                }
-            }
-
-            stage('unit tests') {
-                steps {
-                    echo 'compile'
-                }
-            }
-
-            stage('quality control') {
-                steps {
-                    echo 'compile'
-                }
-            }
-
-            stage('upload code to a centralized place') {
-                steps {
-                    echo 'compile'
-                }
-            }
-
+      stage('unit tests') {
+        steps {
+          echo 'compile'
         }
+      }
+
+      stage('quality control') {
+        steps {
+          echo 'compile'
+        }
+      }
+
+      stage('upload code to a centralized place') {
+        steps {
+          echo 'compile'
+        }
+      }
 
     }
+
+  }
 
 }
 
