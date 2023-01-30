7586a94bcf2e3a540edb8709a1714082c9c6315f
raviteja <ravitejatfcdemo@gmail.com>
2023-01-30 18:22:24 +0530

    adding Jenkinsfile

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -33,6 +33,7 @@ def call() {
             sh "sonar-scanner -Dsonar.host.url=http://mysonarprivateip:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
           }
         }
+        // sed searches for double quotes and remove them
 
         stage('upload code to a centralized place') {
           steps {
