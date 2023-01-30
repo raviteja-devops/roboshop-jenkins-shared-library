b74333aea3ae52639da053ec6cf04d7df2407572
raviteja <ravitejatfcdemo@gmail.com>
2023-01-30 17:47:55 +0530

    adding Jenkinsfile

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -16,7 +16,7 @@ def call() {
           }
         }
 
-        stage('unit tests') {
+        stage('Unit Tests') {
           steps {
             script {
               common.unittests()
@@ -24,9 +24,13 @@ def call() {
           }
         }
 
-        stage('quality control') {
+        stage('Quality Control') {
+          environment {
+            SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+            SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+          }
           steps {
-            echo 'compile'
+            sh "sonar-scanner -Dsonar.host.url=http://mysonarprivateip:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
           }
         }
 
