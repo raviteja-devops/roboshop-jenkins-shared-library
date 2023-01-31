03392fdc10ee56ed33fc9be74ee96088b1a4bfac
raviteja <ravitejatfcdemo@gmail.com>
2023-01-31 19:29:33 +0530

    adding Jenkinsfile

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -28,7 +28,7 @@ def call() {
         SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
         SONAR_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
         wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
-          sh "sonar-scanner -Dsonar.host.url=http://172.31.15.116:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}"
+          sh "sonar-scanner -Dsonar.host.url=http://172.31.15.116:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component} ${SONAR_EXTRA_OPTS}"
         }
       }
 
