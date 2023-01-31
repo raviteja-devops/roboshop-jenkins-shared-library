46d7121c748cf1b2435dd788a117a496097e793a
raviteja <ravitejatfcdemo@gmail.com>
2023-01-31 18:42:39 +0530

    adding Jenkinsfile

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,4 +1,9 @@
 def call() {
+
+  if(!env.SONAR_EXTRA_OPTS) {
+    env.SONAR_EXTRA_OPTS = " "
+  }
+
   try {
     node('work-station') {
 
@@ -23,7 +28,7 @@ def call() {
         SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
         SONAR_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
         wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
-          sh "sonar-scanner -Dsonar.host.url=http://172.31.15.116:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component}"
+          sh "sonar-scanner -Dsonar.host.url=http://172.31.15.116:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}"
         }
       }
 
@@ -39,3 +44,4 @@ def call() {
 
 
 // TRY and CATCH if any exception, anywhere in the entire pipeline, it will mail Failed message and links
+// need to pass extra parameter only for shipping component, -Dsonar.java.binaries=./target
