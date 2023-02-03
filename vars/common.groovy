dbd03dba4e4dd900ba19e736e7c95e1d998f0e3c
raviteja <ravitejatfcdemo@gmail.com>
2023-02-03 11:35:52 +0530

    adding Jenkinsfile

--- vars/common.groovy
+++ vars/common.groovy
@@ -49,9 +49,13 @@ def email(email_note) {
 def artifactPush() {
   sh "echo ${TAG_NAME} >VERSION"
   if (app_lang == "nodejs") {
-    sh "zip -r cart-${TAG_NAME}.zip node_modules server.js VERSION"
+    sh "zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION"
   }
   sh 'ls -l'
+  NEXUS_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names nexus.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+  NEXUS_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names nexus.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+  wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${NEXUS_PASS}", var: 'SECRET']]]) {
+  sh "curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip http://172.31.3.231:8081/repository/${component}/${component}-${TAG_NAME}.zip"
 }
 // we need to compile code but NodeJS is scripting language, no compile is required but add dependencies
 // 'npm install' is going to bring one folder- node_modules, has all dependencies for code to run 'server.js'
