02bd6dd36f3707036d709d08d599905ffa0f7ce4
raviteja <ravitejatfcdemo@gmail.com>
2023-02-03 12:31:07 +0530

    adding Jenkinsfile

--- vars/common.groovy
+++ vars/common.groovy
@@ -48,9 +48,15 @@ def email(email_note) {
 
 def artifactPush() {
   sh "echo ${TAG_NAME} >VERSION"
+
   if (app_lang == "nodejs") {
     sh "zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION ${extraFiles}"
   }
+
+  if (app_lang == "nginx") {
+    sh "zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile"
+  }
+// in nginx we take all the files except Jenkinsfile into zip folder
   sh 'ls -l'
   NEXUS_PASS = sh(script: 'aws ssm get-parameters --region us-east-1 --names nexus.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
   NEXUS_USER = sh(script: 'aws ssm get-parameters --region us-east-1 --names nexus.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
