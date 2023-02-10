c40afd2fe0b1a523be18b2cd61e6b1030965f061
raviteja <ravitejatfcdemo@gmail.com>
2023-02-10 18:24:11 +0530

    Terraform

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -23,6 +23,12 @@ def call() {
         }
       }
 
+      stage('Terraform Apply') {
+        steps {
+          sh "terraform apply -auto-approve -var-file=env-${INFRA_ENV}/main.tfvars"
+        }
+      }
+
     }
 
   }
