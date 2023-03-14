9351281ee342f7509cc66ef4823ee080f91e9a8c
raviteja <ravitejatfcdemo@gmail.com>
2023-03-14 09:34:52 +0530

    Terraform

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -24,8 +24,13 @@ def call() {
                 steps {
                     sh '''
             aws ssm put-parameter --name "${APP_ENV}.${COMPONENT}.APP_VERSION" --type "String" --value "${APP_VERSION}" --overwrite
-            aws ec2 describe-instances     --filters "Name=tag:Name,Values=${APP_ENV}-${COMPONENT}"  | jq ".Reservations[].Instances[].PrivateIpAddress" >/tmp/hosts
-            ansible-playbook -i /tmp/hosts deploy.yml -e component=${COMPONENT} -e env=${APP_ENV} -e ansible_user=${SSH_USR} -e ansible_password=${SSH_PSW}
+            
+            # this is for immutable approach 
+            aws autoscaling start-instance-refresh --auto-scaling-group-name ${APP_ENV}-${COMPONENT}-asg  --preferences '{"InstanceWarmup": 240, "MinHealthyPercentage": 90, "SkipMatching": false}' 
+            
+            ## These are for mutable approach 
+            #aws ec2 describe-instances     --filters "Name=tag:Name,Values=${APP_ENV}-${COMPONENT}"  | jq ".Reservations[].Instances[].PrivateIpAddress" >/tmp/hosts
+            #ansible-playbook -i /tmp/hosts deploy.yml -e component=${COMPONENT} -e env=${APP_ENV} -e ansible_user=${SSH_USR} -e ansible_password=${SSH_PSW}
           '''
                 }
             }
